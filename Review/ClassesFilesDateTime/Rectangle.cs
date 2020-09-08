cba9397b60d4ef612b8de78efaf708f9fa0e3d5e
Brennan Nguyen <brennan.k.nguyen@ou.edu>
2020-09-08 14:25:24 -0500

    Completed the Rectangle class and added a DateTime for the creation

---
+++ Review/ClassesFilesDateTime/Rectangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesFilesDateTime
+{
+    public class Rectangle
+    {
+        public int width { get; set; }
+        public int height{ get; set; }
+
+        public DateTime creationDate { get; set; }
+
+        /// <summary>
+        /// This is the default / empty constructor
+        /// </summary>
+        public Rectangle()
+        {
+            width = 0;
+            height = 0;
+            creationDate = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Overloaded constructor for the Rectangle Class
+        /// </summary>
+        /// <param name="width">An interger representing the width of the Rectangle</param>
+        /// <param name="height">An interger representing the height of the Rectangle</param>
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            creationDate = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Calculate the area of the Rectangle
+        /// </summary>
+        /// <returns>The area of the Rectangle</returns>
+        public double area()
+        {
+            return width * height;
+        }
+
+        /// <summary>
+        /// Overload the ToSring to output what we want about the Rectangle
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            var diff = DateTime.Now - creationDate;
+           string output = $"The rectangle was created {creationDate.DayOfWeek} {diff.TotalSeconds} ago & has a width of {width} and height of {height}" +
+           $"\n\tThe area of the rectangle is {area()}";
+
+            return output;
+        }
+    }
+}
