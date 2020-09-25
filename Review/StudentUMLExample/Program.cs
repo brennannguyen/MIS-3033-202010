f38df47040bee3d360ffed052ed94ae82bce9b7c
Brennan Nguyen <brennan.k.nguyen@ou.edu>
2020-09-24 19:33:12 -0500

    Completed EntryForm Explanation.

---
+++ Review/StudentUMLExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StudentUMLExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student myStudent = new Student();
+            Console.WriteLine(myStudent);
+            myStudent.FirstName = "Adam";
+            myStudent.LastName = "Ackerman";
+            myStudent.IsOnProbation = true;
+            myStudent.SoonerID = 5;
+
+            Console.WriteLine(myStudent);
+
+            myStudent.MakePayment(500);
+            Console.WriteLine(myStudent);
+        }
+    }
+}
